36bf8c115335533788e92cb52cd1cc5df946f61b
sudeep162002 <80563363+sudeep162002@users.noreply.github.com>
2021-10-09 21:57:31 +0530

    mysql connection

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,16 +1,56 @@
 const express = require('express')
+
+var mysql = require('mysql')
 const app = express()
 const port = 3030
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log("connection sucessful")
-})
 
-app.get('/checque', (req, res) => {
-    res.send('checque pass you are great!')
+
+//app.get('/checque', (req, res) => {
+ //   res.send('checque pass you are great!')
+ // })
+
+
+  // my sql connection
+
+  var connection= mysql.createConnection(
+      {
+          //properties
+          host: 'localhost',
+          user: 'root',
+          password: ' ',
+          database: 'sampelDB'
+      }
+  );
+
+  connection.connect(function(error){
+      if(!!error){
+          console.log('error');
+      }
+      else{
+          console.log('connected');
+      }
+  });
+
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+    console.log("connection sucessful")
   })
+
+
+  app.get('/db', (req, res) => {
+    connection.query(" ")
+  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+
+
+
